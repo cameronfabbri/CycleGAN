fbb977c1463a52564d3e03928759ef6dfd1559b9
Cameron Fabbri <cameronfabbri@gmail.com>
2017-07-05 15:34:05 -0500

    update

--- ops/data_ops.py
+++ ops/data_ops.py
@@ -22,7 +22,7 @@ import os
 import fnmatch
 import cPickle as pickle
 
-Data = collections.namedtuple('trainData', 'paths, inputs, targets, count, steps_per_epoch')
+Data = collections.namedtuple('trainData', 'inputsA, inputsB, count')
 
 def preprocess(image):
     with tf.name_scope('preprocess'):
@@ -76,86 +76,6 @@ def check_image(image):
     image.set_shape(shape)
     return image
 
-# based on https://github.com/torch/image/blob/9f65c30167b2048ecbe8b7befdc6b2d6d12baee9/generic/image.c
-def rgb_to_lab(srgb):
-    with tf.name_scope('rgb_to_lab'):
-        srgb = check_image(srgb)
-        srgb_pixels = tf.reshape(srgb, [-1, 3])
-        with tf.name_scope('srgb_to_xyz'):
-            linear_mask = tf.cast(srgb_pixels <= 0.04045, dtype=tf.float32)
-            exponential_mask = tf.cast(srgb_pixels > 0.04045, dtype=tf.float32)
-            rgb_pixels = (srgb_pixels / 12.92 * linear_mask) + (((srgb_pixels + 0.055) / 1.055) ** 2.4) * exponential_mask
-            rgb_to_xyz = tf.constant([
-                #    X        Y          Z
-                [0.412453, 0.212671, 0.019334], # R
-                [0.357580, 0.715160, 0.119193], # G
-                [0.180423, 0.072169, 0.950227], # B
-            ])
-            xyz_pixels = tf.matmul(rgb_pixels, rgb_to_xyz)
-
-        # https://en.wikipedia.org/wiki/Lab_color_space#CIELAB-CIEXYZ_conversions
-        with tf.name_scope('xyz_to_cielab'):
-            # convert to fx = f(X/Xn), fy = f(Y/Yn), fz = f(Z/Zn)
-
-            # normalize for D65 white point
-            xyz_normalized_pixels = tf.multiply(xyz_pixels, [1/0.950456, 1.0, 1/1.088754])
-
-            epsilon = 6/29
-            linear_mask = tf.cast(xyz_normalized_pixels <= (epsilon**3), dtype=tf.float32)
-            exponential_mask = tf.cast(xyz_normalized_pixels > (epsilon**3), dtype=tf.float32)
-            fxfyfz_pixels = (xyz_normalized_pixels / (3 * epsilon**2) + 4/29) * linear_mask + (xyz_normalized_pixels ** (1/3)) * exponential_mask
-
-            # convert to lab
-            fxfyfz_to_lab = tf.constant([
-                #  l       a       b
-                [  0.0,  500.0,    0.0], # fx
-                [116.0, -500.0,  200.0], # fy
-                [  0.0,    0.0, -200.0], # fz
-            ])
-            lab_pixels = tf.matmul(fxfyfz_pixels, fxfyfz_to_lab) + tf.constant([-16.0, 0.0, 0.0])
-
-        return tf.reshape(lab_pixels, tf.shape(srgb))
-
-
-def lab_to_rgb(lab):
-    with tf.name_scope('lab_to_rgb'):
-        lab = check_image(lab)
-        lab_pixels = tf.reshape(lab, [-1, 3])
-        # https://en.wikipedia.org/wiki/Lab_color_space#CIELAB-CIEXYZ_conversions
-        with tf.name_scope('cielab_to_xyz'):
-            # convert to fxfyfz
-            lab_to_fxfyfz = tf.constant([
-                #   fx      fy        fz
-                [1/116.0, 1/116.0,  1/116.0], # l
-                [1/500.0,     0.0,      0.0], # a
-                [    0.0,     0.0, -1/200.0], # b
-            ])
-            fxfyfz_pixels = tf.matmul(lab_pixels + tf.constant([16.0, 0.0, 0.0]), lab_to_fxfyfz)
-
-            # convert to xyz
-            epsilon = 6/29
-            linear_mask = tf.cast(fxfyfz_pixels <= epsilon, dtype=tf.float32)
-            exponential_mask = tf.cast(fxfyfz_pixels > epsilon, dtype=tf.float32)
-            xyz_pixels = (3 * epsilon**2 * (fxfyfz_pixels - 4/29)) * linear_mask + (fxfyfz_pixels ** 3) * exponential_mask
-
-            # denormalize for D65 white point
-            xyz_pixels = tf.multiply(xyz_pixels, [0.950456, 1.0, 1.088754])
-
-        with tf.name_scope('xyz_to_srgb'):
-            xyz_to_rgb = tf.constant([
-                #     r           g          b
-                [ 3.2404542, -0.9692660,  0.0556434], # x
-                [-1.5371385,  1.8760108, -0.2040259], # y
-                [-0.4985314,  0.0415560,  1.0572252], # z
-            ])
-            rgb_pixels = tf.matmul(xyz_pixels, xyz_to_rgb)
-            # avoid a slightly negative number messing up the conversion
-            rgb_pixels = tf.clip_by_value(rgb_pixels, 0.0, 1.0)
-            linear_mask = tf.cast(rgb_pixels <= 0.0031308, dtype=tf.float32)
-            exponential_mask = tf.cast(rgb_pixels > 0.0031308, dtype=tf.float32)
-            srgb_pixels = (rgb_pixels * 12.92 * linear_mask) + ((rgb_pixels ** (1/2.4) * 1.055) - 0.055) * exponential_mask
-
-        return tf.reshape(srgb_pixels, tf.shape(lab))
 
 def getPaths(data_dir, gray_images=None, ext='jpg'):
    pattern   = '*.'+ext
@@ -172,257 +92,80 @@ def getPaths(data_dir, gray_images=None, ext='jpg'):
 
 
 # TODO add in files to exclude (gray ones)
-def loadData(data_dir, dataset, batch_size, jitter=True, train=True, SIZE=256):
+def loadData(dataset, batch_size, train=True):
 
-   if data_dir is None or not os.path.exists(data_dir):
-      raise Exception('data_dir does not exist')
+   # data directory plus the dataset being used
+   data_dir = '/mnt/data2/images/cyclegan/datasets/'+dataset+'/'
 
-   if dataset == 'true_gray':
-      train_paths = []
-      test_paths  = pickle.load(open('/home/fabbric/Research/colorgans/files/true_gray/true_gray.pkl', 'rb'))
+   # check to see if dataset actually exists
+   if dataset is None or not os.path.exists(data_dir):
+      raise Exception('data_dir '+data_dir+' does not exist')
 
-   if dataset == 'looneytunes':
-      print 'Using Looney Tunes'
-      pkl_train_file = 'files/looneytunes_train.pkl'
-      pkl_test_file  = 'files/looneytunes_test.pkl'
+   print 'Getting data...'
 
-      if os.path.isfile(pkl_train_file) and os.path.isfile(pkl_test_file):
-         print 'Found pickle file'
-         train_paths = pickle.load(open(pkl_train_file, 'rb'))
-      else:
-         train_dir = data_dir
-         train_paths = getPaths(train_dir, gray_images=None)
-         random.shuffle(train_paths)
-         pf   = open(pkl_train_file, 'wb')
-         data = pickle.dumps(train_paths)
-         pf.write(data)
-         pf.close()
-
-   if dataset == 'stl10':
-      print 'Using stl10'
-      pkl_train_file = 'files/stl10_train.pkl'
-      pkl_test_file  = 'files/stl10_test.pkl'
-
-      if os.path.isfile(pkl_train_file) and os.path.isfile(pkl_test_file):
-         print 'Found pickle file'
-         train_paths = pickle.load(open(pkl_train_file, 'rb'))
-         test_paths  = pickle.load(open(pkl_test_file, 'rb'))
-         test_paths = test_paths[54:58]
-      else:
-         train_dir = data_dir+'train/'
-         test_dir  = data_dir+'test/'
-         train_paths = getPaths(train_dir, gray_images=None)
-         test_paths  = getPaths(test_dir, gray_images=None)
-         random.shuffle(train_paths)
-         random.shuffle(test_paths)
-         pf   = open(pkl_train_file, 'wb')
-         data = pickle.dumps(train_paths)
-         pf.write(data)
-         pf.close()
-         pf   = open(pkl_test_file, 'wb')
-         data = pickle.dumps(test_paths)
-         pf.write(data)
-         pf.close()
-   if dataset == 'celeba':
-      print 'Using celeba'
-      pkl_train_file = 'files/celeba_train.pkl'
-      pkl_test_file  = 'files/celeba_test.pkl'
-
-      if os.path.isfile(pkl_train_file) and os.path.isfile(pkl_test_file):
-         print 'Found pickle file'
-         train_paths = pickle.load(open(pkl_train_file, 'rb'))
-         test_paths  = pickle.load(open(pkl_test_file, 'rb'))
-         # ADDED FOR TESTING ONLY
-         #test_paths = test_paths[54:58]
-         test_paths = test_paths[2354:2358]
-      else:
-         image_paths = getPaths(data_dir)
-         random.shuffle(image_paths)
-
-         train_paths = image_paths[:195000]
-         test_paths  = image_paths[195000:]
-
-         pf   = open(pkl_train_file, 'wb')
-         data = pickle.dumps(train_paths)
-         pf.write(data)
-         pf.close()
-         
-         pf   = open(pkl_test_file, 'wb')
-         data = pickle.dumps(test_paths)
-         pf.write(data)
-         pf.close()
-   if dataset == 'imagenet':
-      print 'Using imagenet'
-      pkl_train_file = 'files/imagenet_train.pkl'
-      pkl_test_file  = 'files/imagenet_test.pkl'
-      if os.path.isfile(pkl_train_file) and os.path.isfile(pkl_test_file):
-         print 'Found pickle file, loading data...'
-         train_paths = pickle.load(open(pkl_train_file, 'rb'))
-         test_paths  = pickle.load(open(pkl_test_file, 'rb'))
-      else:
-         #train_dir = data_dir+'train/'
-         train_dir = data_dir
-         test_dir  = data_dir
-         #test_dir  = data_dir+'test/'
-         #train_paths = getPaths(train_dir, ext='JPEG')
-         train_paths = getPaths(train_dir,ext='png')
-         test_paths  = getPaths(test_dir,ext='png')
-         #test_paths  = getPaths(test_dir, ext='JPEG')
-         random.shuffle(train_paths)
-         random.shuffle(test_paths)
-         pf   = open(pkl_train_file, 'wb')
-         data = pickle.dumps(train_paths)
-         pf.write(data)
-         pf.close()
-         pf   = open(pkl_test_file, 'wb')
-         data = pickle.dumps(test_paths)
-         pf.write(data)
-         pf.close()
-   if dataset == 'places2_standard':
-      gray_images = []
-      with open('files/gray_images_standard.txt','r') as f:
-         for line in f:
-            line = line.rstrip()
-            gray_images.append(line)
-      print 'Using places2 standard'
-      pkl_train_file = 'files/places2_standard_train.pkl'
-      pkl_test_file  = 'files/places2_standard_test.pkl'
-
-      if os.path.isfile(pkl_train_file) and os.path.isfile(pkl_test_file):
-         print 'Found pickle file, loading data...'
-         train_paths = pickle.load(open(pkl_train_file, 'rb'))
-         test_paths  = pickle.load(open(pkl_test_file, 'rb'))
-      else:
-         train_dir = data_dir+'train_256/'
-         test_dir  = data_dir+'test_256/'
-         train_paths = getPaths(train_dir, gray_images=gray_images)
-         test_paths  = getPaths(test_dir, gray_images=gray_images)
-         random.shuffle(train_paths)
-         random.shuffle(test_paths)
-         pf   = open(pkl_train_file, 'wb')
-         data = pickle.dumps(train_paths)
-         pf.write(data)
-         pf.close()
-         pf   = open(pkl_test_file, 'wb')
-         data = pickle.dumps(test_paths)
-         pf.write(data)
-         pf.close()
-   if dataset == 'places2_challenge':
-      print 'Using places2 challenge'
-      pkl_train_file = 'files/places2_challenge_train.pkl'
-      pkl_test_file  = 'files/places2_challenge_test.pkl'
-
-      if os.path.isfile(pkl_train_file) and os.path.isfile(pkl_test_file):
-         print 'Found pickle file, loading data...'
-         train_paths = pickle.load(open(pkl_train_file, 'rb'))
-         test_paths  = pickle.load(open(pkl_test_file, 'rb'))
-      else:
-         train_dir = data_dir+'train_256/'
-         test_dir  = data_dir+'test_256/'
-         train_paths = getPaths(train_dir)
-         test_paths  = getPaths(test_dir)
-         random.shuffle(train_paths)
-         random.shuffle(test_paths)
-         pf   = open(pkl_train_file, 'wb')
-         data = pickle.dumps(train_paths)
-         pf.write(data)
-         pf.close()
-         pf   = open(pkl_test_file, 'wb')
-         data = pickle.dumps(test_paths)
-         pf.write(data)
-         pf.close()
-
-   if dataset == 'bedroom':
-      pkl_train_file = 'files/bedroom_train.pkl'
-      pkl_test_file  = 'files/bedroom_test.pkl'
-      if os.path.isfile(pkl_train_file) and os.path.isfile(pkl_test_file):
-         print 'Found pickle file'
-         train_paths = pickle.load(open(pkl_train_file, 'rb'))
-         test_paths  = pickle.load(open(pkl_test_file, 'rb'))
-      else:   
-         print 'Using lsun subset bedroom'
-         train_dir = data_dir+'images/bedroom/train/'
-         test_dir  = data_dir+'images/bedroom/val/'
-         train_paths = getPaths(train_dir)
-         test_paths  = getPaths(test_dir)
-         random.shuffle(train_paths)
-         random.shuffle(test_paths)
-         pf   = open(pkl_train_file, 'wb')
-         data = pickle.dumps(train_paths)
-         pf.write(data)
-         pf.close()
-         pf   = open(pkl_test_file, 'wb')
-         data = pickle.dumps(test_paths)
-         pf.write(data)
-         pf.close()
-
-   print 'Done!'
-   if train: input_paths = train_paths
+   if train:
+      imageA_dir = data_dir+'trainA/'
+      imageB_dir = data_dir+'trainB/'
    else:
-      #random.shuffle(test_paths)
-      #input_paths = test_paths[123:127]
-      input_paths = test_paths
-   print len(input_paths),'images!'
+      imageA_dir = data_dir+'testA/'
+      imageB_dir = data_dir+'testB/'
+      
+   imageA_paths = getPaths(imageA_dir)
+   imageB_paths = getPaths(imageB_dir)
+   random.shuffle(imageA_paths)
+   random.shuffle(imageB_paths)
+
+   if len(imageA_paths) == 0:
+      raise Exception('data_dir contains no image files')
+   print 'Done!'
+
+   print len(imageA_paths)+len(imageB_paths),'images!'
    decode = tf.image.decode_image
    
-   if len(input_paths) == 0:
-      raise Exception('data_dir contains no image files')
-
    with tf.name_scope('load_images'):
-      path_queue = tf.train.string_input_producer(input_paths, shuffle=True)
+
+      # create a queue from the input paths
+      pathA_queue = tf.train.string_input_producer(imageA_paths, shuffle=True)
+      pathB_queue = tf.train.string_input_producer(imageB_paths, shuffle=True)
+
+      # read in contents from the path
       reader = tf.WholeFileReader()
-      paths, contents = reader.read(path_queue)
-      raw_input = decode(contents)
-      raw_input = tf.image.convert_image_dtype(raw_input, dtype=tf.float32)
+      pathsA, contentsA = reader.read(pathA_queue)
+      pathsB, contentsB = reader.read(pathB_queue)
+      
+      # decode image, then convert to float32
+      inputsA = tf.image.convert_image_dtype(decode(contentsA), dtype=tf.float32)
+      inputsB = tf.image.convert_image_dtype(decode(contentsB), dtype=tf.float32)
 
-      assertion = tf.assert_equal(tf.shape(raw_input)[2], 3, message='image does not have 3 channels')
-      with tf.control_dependencies([assertion]):
-         raw_input = tf.identity(raw_input)
+      inputsA.set_shape([256, 256, 3])
+      inputsB.set_shape([256, 256, 3])
 
-      raw_input.set_shape([None, None, 3])
-
-      # load color and brightness from image, no B image exists here
-      lab = rgb_to_lab(raw_input)
-      L_chan, a_chan, b_chan = preprocess_lab(lab)
-      a_images = tf.expand_dims(L_chan, axis=2)
-      b_images = tf.stack([a_chan, b_chan], axis=2)
-    
-   inputs, targets = [a_images, b_images]
-
-   # synchronize seed for image operations so that we do the same operations to both
-   # input and output images
-   flip = 1
-   scale_size = SIZE+20
-   CROP_SIZE  = SIZE
-
+   # randomly flip and rescale images -> increse image size by 20 pixels then crop it back to 256x256
+   scale_size = 256+20
+   crop_size  = 256
    seed = random.randint(0, 2**31 - 1) 
    def transform(image):
       r = image
       r = tf.image.random_flip_left_right(r, seed=seed)
 
-      # area produces a nice downscaling, but does nearest neighbor for upscaling
-      # assume we're going to be doing downscaling here
       r = tf.image.resize_images(r, [scale_size, scale_size], method=tf.image.ResizeMethod.AREA)
-      offset = tf.cast(tf.floor(tf.random_uniform([2], 0, scale_size - CROP_SIZE + 1, seed=seed)), dtype=tf.int32)
-      r = tf.image.crop_to_bounding_box(r, offset[0], offset[1], CROP_SIZE, CROP_SIZE)
+      offset = tf.cast(tf.floor(tf.random_uniform([2], 0, scale_size - crop_size + 1, seed=seed)), dtype=tf.int32)
+      r = tf.image.crop_to_bounding_box(r, offset[0], offset[1], crop_size, crop_size)
       return r
 
-   if train and jitter:
-      with tf.name_scope('input_images'):
-         input_images = transform(inputs)
-      with tf.name_scope('target_images'):
-         target_images = transform(targets)
+   if train:
+      with tf.name_scope('inputsA'):
+         inputsA = transform(inputsA)
+      with tf.name_scope('inputsB'):
+         inputsB = transform(inputsB)
    else:
-      input_images = tf.image.resize_images(inputs, [CROP_SIZE, CROP_SIZE], method=tf.image.ResizeMethod.AREA)
-      target_images = tf.image.resize_images(targets, [CROP_SIZE, CROP_SIZE], method=tf.image.ResizeMethod.AREA)
+      input_images = tf.image.resize_images(inputs, [crop_size, crop_size], method=tf.image.ResizeMethod.AREA)
+      target_images = tf.image.resize_images(targets, [crop_size, crop_size], method=tf.image.ResizeMethod.AREA)
 
-   paths_batch, inputs_batch, targets_batch = tf.train.batch([paths, input_images, target_images], batch_size=batch_size)
-   steps_per_epoch = int(math.ceil(len(input_paths) / batch_size))
+   inputsA_batch, inputsB_batch = tf.train.batch([inputsA, inputsB], batch_size=batch_size)
 
    return Data(
-      paths=paths_batch,
-      inputs=inputs_batch,
-      targets=targets_batch,
-      count=len(input_paths),
-      steps_per_epoch=steps_per_epoch,
+      inputsA=inputsA_batch,
+      inputsB=inputsB_batch,
+      count=len(imageA_paths)+len(imageB_paths),
    )
