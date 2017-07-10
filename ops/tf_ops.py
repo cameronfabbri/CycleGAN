4be0a431bef456d4c20e1fe2362e04a7585daa79
Cameron Fabbri <cameronfabbri@gmail.com>
2017-07-10 11:31:18 -0500

    model files done I think

--- ops/tf_ops.py
+++ ops/tf_ops.py
@@ -9,35 +9,19 @@ import numpy as np
 import math
 
 '''
+   Batch normalization
 '''
-def batch_norm(x):
-    with tf.variable_scope('batchnorm'):
-      x = tf.identity(x)
+def bn(x):
+   return tf.layers.batch_normalization(x)
 
-      channels = x.get_shape()[3]
-      offset = tf.get_variable('offset', [channels], dtype=tf.float32, initializer=tf.zeros_initializer())
-      scale = tf.get_variable('scale', [channels], dtype=tf.float32, initializer=tf.random_normal_initializer(1.0, 0.02))
-      mean, variance = tf.nn.moments(x, axes=[0, 1, 2], keep_dims=False)
-      variance_epsilon = 1e-5
-      normalized = tf.nn.batch_normalization(x, mean, variance, offset, scale, variance_epsilon=variance_epsilon)
-      return normalized
 
-def conv2d(x, out_channels, stride=2,kernel_size=4):
-   with tf.variable_scope('conv2d'):
-      in_channels = x.get_shape()[3]
-      kernel = tf.get_variable('kernel', [kernel_size, kernel_size, in_channels, out_channels], dtype=tf.float32, initializer=tf.random_normal_initializer(0, 0.02))
-      # [batch, in_height, in_width, in_channels], [kernel_width, kernel_height, in_channels, out_channels]
-      #     => [batch, out_height, out_width, out_channels]
-      padded_input = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='CONSTANT')
-      conv = tf.nn.conv2d(padded_input, kernel, [1, stride, stride, 1], padding='VALID')
-      return conv
-
-def conv2d_transpose(x, out_channels, stride=2, kernel_size=4):
-   with tf.variable_scope('conv2d_transpose'):
-      batch, in_height, in_width, in_channels = [int(d) for d in x.get_shape()]
-      kernel = tf.get_variable('kernel', [kernel_size, kernel_size, out_channels, in_channels], dtype=tf.float32, initializer=tf.random_normal_initializer(0, 0.02))
-      conv = tf.nn.conv2d_transpose(x, kernel, [batch, in_height * 2, in_width * 2, out_channels], [1, stride, stride, 1], padding='SAME')
-      return conv
+'''
+   Instance normalization
+'''
+def instance_norm(x):
+   epsilon = 1e-5
+   mean, var = tf.nn.moments(x, [1, 2], keep_dims=True)
+   return tf.div(tf.subtract(x, mean), tf.sqrt(tf.add(var, epsilon)))
 
 
 ######## activation functions ###########
--- train.py
+++ train.py
@@ -9,20 +9,22 @@ import os
 import time
 
 sys.path.insert(0, 'ops/')
+sys.path.insert(0, 'nets/')
 
 from tf_ops import *
 import data_ops
 
 if __name__ == '__main__':
    parser = argparse.ArgumentParser()
-   parser.add_argument('--ARCHITECTURE', required=True,help='Architecture for the generator')
+   parser.add_argument('--ARCHITECTURE', required=False,default='resnet',help='Architecture for the generator')
    parser.add_argument('--EPOCHS',       required=False,default=100,type=int,help='Number of epochs for GAN')
    parser.add_argument('--DATASET',      required=True,help='The dataset to use')
    parser.add_argument('--LEARNING_RATE',required=False,default=0.0002,type=float,help='Learning rate for the pretrained network')
-   parser.add_argument('--BATCH_SIZE',   required=False,type=int,default=32,help='Batch size to use')
-   parser.add_argument('--LOSS_METHOD',  required=False,default='wasserstein',help='Loss function for GAN',
-      choices=['wasserstein','least_squares','energy','gan','cnn'])
-   parser.add_argument('--CYCLE_WEIGHT', required=False,help='weight of L2 for combined loss',type=float,default=0.0)
+   parser.add_argument('--BATCH_SIZE',   required=False,type=int,default=4,help='Batch size to use')
+   parser.add_argument('--LOSS_METHOD',  required=False,default='least_squares',help='Loss function for GAN',
+      choices=['wasserstein','least_squares','gan'])
+   parser.add_argument('--CYCLE_WEIGHT', required=False,help='weight of the cycle loss',type=float,default=10.0)
+   parser.add_argument('--HISTORY',      required=False,help='size of history',type=int,default=50)
    a = parser.parse_args()
 
    ARCHITECTURE  = a.ARCHITECTURE
@@ -32,7 +34,9 @@ if __name__ == '__main__':
    BATCH_SIZE    = a.BATCH_SIZE
    LOSS_METHOD   = a.LOSS_METHOD
    CYCLE_WEIGHT  = a.CYCLE_WEIGHT
-   
+
+   import resnet as network
+
    EXPERIMENT_DIR = 'checkpoints/ARCHITECTURE_'+ARCHITECTURE+'/DATASET_'+DATASET+'/LEARNING_RATE_'+str(LEARNING_RATE)+'/LOSS_'+LOSS_METHOD+'/CYCLE_'+str(CYCLE_WEIGHT)+'/'
    print EXPERIMENT_DIR
    
@@ -86,80 +90,55 @@ if __name__ == '__main__':
    # The image from data domain B
    inputsB = Data.inputsB
 
-   # TODO it's actually two different generators, not just one
-   # transform domain A to domain B, then back to domain A, minimize L1
-   AtoB = network.netG(inputsA)
-   BtoA = network.netG(inputsB)
+   # generate images
+   fakeB = network.netG(inputsA, 'atob1')
+   fakeA = network.netG(inputsB, 'btoa1')
 
-   Arecon = network.netG(BtoA)
-   Brecon = network.netG(AtoB)
+   # cycle part for reconstruction
+   Arecon = network.netG(fakeB, 'btoa2')
+   Brecon = network.netG(fakeA, 'atob2')
 
+   # L1 loss for cycles
    AR_loss = tf.reduce_mean(tf.abs(Arecon-inputsA))
    BR_loss = tf.reduce_mean(tf.abs(Brecon-inputsB))
 
+   # total cycle loss
    L_cyc = tf.reduce_mean(tf.abs(AR_loss-BR_loss))
 
+   # send the real domain A images to d
+   discA = network.netD(inputsA, 'domain_a')
+
+   # send the real domain images B to d
+   discB = network.netD(inputsA, 'domain_b')
+
+   # send the generated domain A images to d
+   discA_gen = network.netD(fakeA, 'domain_a', reuse=True)
+   
+   # send the generated domain B images to d
+   discB_gen = network.netD(fakeA, 'domain_b', reuse=True)
+
+   D_real = discA+discB
+   D_fake = discA_gen+discB_gen
+   
+   # added for safe log
    e = 1e-12
    if LOSS_METHOD == 'least_squares':
       print 'Using least squares loss'
-      # Least squares requires sigmoid activation on D
+
       errD_real = tf.nn.sigmoid(D_real)
       errD_fake = tf.nn.sigmoid(D_fake)
       
-      gen_loss_GAN = 0.5*(tf.reduce_mean(tf.square(errD_fake - 1)))
-      if L1_WEIGHT > 0.0:
-         print 'Using an L1 weight of',L1_WEIGHT
-         gen_loss_L1  = tf.reduce_mean(tf.abs(ab_image-gen_ab))
-         errG         = gen_loss_GAN*GAN_WEIGHT + gen_loss_L1*L1_WEIGHT
-      if L2_WEIGHT > 0.0:
-         print 'Using an L2 weight of',L2_WEIGHT
-         gen_loss_L2  = tf.reduce_mean(tf.nn.l2_loss(ab_image-gen_ab))
-         errG         = gen_loss_GAN*GAN_WEIGHT + gen_loss_L2*L2_WEIGHT
-      elif L1_WEIGHT <= 0.0 and L2_WEIGHT <= 0.0:
-         print 'Just using GAN loss, no L1 or L2'
-         errG = gen_loss_GAN
+      errG = 0.5*(tf.reduce_mean(tf.square(errD_fake - 1)))
       errD = tf.reduce_mean(0.5*(tf.square(errD_real - 1)) + 0.5*(tf.square(errD_fake)))
+
    if LOSS_METHOD == 'gan':
       print 'Using original GAN loss'
-      if LOSS_METHOD is not 'cnn':
-         D_real = tf.nn.sigmoid(D_real)
-         D_fake = tf.nn.sigmoid(D_fake)
-         gen_loss_GAN = tf.reduce_mean(-tf.log(D_fake + e))
-      else: gen_loss_GAN = 0.0
-      if L1_WEIGHT > 0.0:
-         print 'Using an L1 weight of',L1_WEIGHT
-         gen_loss_L1  = tf.reduce_mean(tf.abs(ab_image-gen_ab))
-         errG         = gen_loss_GAN*GAN_WEIGHT + gen_loss_L1*L1_WEIGHT
-      if L2_WEIGHT > 0.0:
-         print 'Using an L2 weight of',L2_WEIGHT
-         gen_loss_L2  = tf.reduce_mean(tf.nn.l2_loss(ab_image-gen_ab))
-         errG         = gen_loss_GAN*GAN_WEIGHT + gen_loss_L2*L2_WEIGHT
-      if L1_WEIGHT <= 0.0 and L2_WEIGHT <= 0.0:
-         print 'Just using GAN loss, no L1 or L2'
-         #errD = errD + e
-         errG = gen_loss_GAN
+      D_real = tf.nn.sigmoid(D_real)
+      D_fake = tf.nn.sigmoid(D_fake)
+      errG = tf.reduce_mean(-tf.log(D_fake + e))
+
       errD = tf.reduce_mean(-(tf.log(D_real+e)+tf.log(1-D_fake+e)))
    
-   if LOSS_METHOD == 'energy':
-      print 'Using energy loss'
-      margin = 80
-      gen_loss_GAN = D_fake
-
-      zero = tf.zeros_like(margin-D_fake)
-
-      if L1_WEIGHT > 0.0:
-         print 'Using an L1 weight of',L1_WEIGHT
-         gen_loss_L1 = tf.reduce_mean(tf.abs(ab_image-gen_ab))
-         errG        = gen_loss_GAN*GAN_WEIGHT + gen_loss_L1*L1_WEIGHT
-      if L2_WEIGHT > 0.0:
-         print 'Using an L2 weight of',L2_WEIGHT
-         gen_loss_L2 = tf.reduce_mean(tf.nn.l2_loss(ab_image-gen_ab))
-         errG        = gen_loss_GAN*GAN_WEIGHT + gen_loss_L2*L2_WEIGHT
-      if L1_WEIGHT <= 0.0 and L2_WEIGHT <= 0.0:
-         print 'Just using energy loss, no L1 or L2'
-         errG = gen_loss_GAN
-      errD = D_real + tf.maximum(zero, margin-D_fake)
-
    # tensorboard summaries
    try: tf.summary.scalar('d_loss', tf.reduce_mean(errD))
    except:pass
@@ -168,28 +147,16 @@ if __name__ == '__main__':
 
    # get all trainable variables, and split by network G and network D
    t_vars = tf.trainable_variables()
-   d_vars = [var for var in t_vars if 'd_' in var.name]
-   g_vars = [var for var in t_vars if 'g_' in var.name]
+   d_vars = [var for var in t_vars if 'd-' in var.name]
+   g_vars = [var for var in t_vars if 'g-' in var.name]
 
    if LOSS_METHOD == 'wasserstein':
       # clip weights in D
       clip_values = [-0.005, 0.005]
       clip_discriminator_var_op = [var.assign(tf.clip_by_value(var, clip_values[0], clip_values[1])) for var in d_vars]
-
-   # MSE loss for pretraining
-   if EPOCHS > 0:
-      print 'Pretraining generator for',EPOCHS,'epochs...'
-      mse_loss = tf.reduce_mean((ab_image-gen_ab)**2)
-      #mse_train_op = tf.train.AdamOptimizer(learning_rate=PRETRAIN_LR,beta1=0.5).minimize(mse_loss, var_list=g_vars, global_step=global_step, colocate_gradients_with_ops=True)
-      mse_train_op = tf.train.AdamOptimizer(learning_rate=PRETRAIN_LR).minimize(mse_loss, var_list=g_vars, global_step=global_step, colocate_gradients_with_ops=True)
-      tf.add_to_collection('vars', mse_train_op)
-      tf.summary.scalar('mse_loss', mse_loss)
-   if LOSS_METHOD == 'wasserstein':
-      G_train_op = tf.train.RMSPropOptimizer(learning_rate=LEARNING_RATE, decay=0.9).minimize(errG, var_list=g_vars, global_step=global_step, colocate_gradients_with_ops=True)
-      D_train_op = tf.train.RMSPropOptimizer(learning_rate=LEARNING_RATE, decay=0.9).minimize(errD, var_list=d_vars, colocate_gradients_with_ops=True)
+      G_train_op = tf.train.RMSPropOptimizer(learning_rate=LEARNING_RATE).minimize(errG, var_list=g_vars, global_step=global_step, colocate_gradients_with_ops=True)
+      D_train_op = tf.train.RMSPropOptimizer(learning_rate=LEARNING_RATE).minimize(errD, var_list=d_vars, global_step=global_step, colocate_gradients_with_ops=True)
    else:
-      #G_train_op = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE,beta1=0.5).minimize(errG, var_list=g_vars, global_step=global_step, colocate_gradients_with_ops=True)
-      #D_train_op = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE,beta1=0.5).minimize(errD, var_list=d_vars, colocate_gradients_with_ops=True)
       G_train_op = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(errG, var_list=g_vars, global_step=global_step, colocate_gradients_with_ops=True)
       D_train_op = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(errD, var_list=d_vars, colocate_gradients_with_ops=True)
 
@@ -205,7 +172,6 @@ if __name__ == '__main__':
    tf.add_to_collection('vars', G_train_op)
    tf.add_to_collection('vars', D_train_op)
 
-   # only keep one model
    ckpt = tf.train.get_checkpoint_state(EXPERIMENT_DIR)
    # restore previous model if there is one
    if ckpt and ckpt.model_checkpoint_path:
@@ -216,15 +182,7 @@ if __name__ == '__main__':
       except:
          print "Could not restore model"
          pass
-   if LOAD_MODEL:
-      ckpt = tf.train.get_checkpoint_state(LOAD_MODEL)
-      print "Restoring model..."
-      try:
-         saver.restore(sess, ckpt.model_checkpoint_path)
-         print "Model restored"
-      except:
-         print "Could not restore model"
-         raise
+   
    ########################################### training portion
    step = sess.run(global_step)
    coord = tf.train.Coordinator()
@@ -236,29 +194,12 @@ if __name__ == '__main__':
       while epoch_num < EPOCHS:
          epoch_num = step/(num_train/BATCH_SIZE)
          s = time.time()
-         sess.run(mse_train_op)
-         mse, summary = sess.run([mse_loss, merged_summary_op])
-         step += 1
-         summary_writer.add_summary(summary, step)
-         print 'step:',step,'mse:',mse,'time:',time.time()-s
-         if step % 500 == 0:
-            print
-            print 'Saving model'
-            print
-            saver.save(sess, EXPERIMENT_DIR+'checkpoint-'+str(step))
-            saver.export_meta_graph(EXPERIMENT_DIR+'checkpoint-'+str(step)+'.meta')
-      if EPOCHS > 0:
-         print 'Done pretraining....training D and G now'
-         epoch_num = 0
-      while epoch_num < EPOCHS:
-         epoch_num = step/(num_train/BATCH_SIZE)
-         s = time.time()
 
          if LOSS_METHOD == 'wasserstein':
             if step < 10 or step % 500 == 0:
                n_critic = 100
             else: n_critic = NUM_CRITIC
-            for critic_itr in range(n_critic):
+            for critic_itr in range(5):
                sess.run(D_train_op)
                sess.run(clip_discriminator_var_op)
             sess.run(G_train_op)
@@ -268,18 +209,9 @@ if __name__ == '__main__':
             sess.run(D_train_op)
             sess.run(G_train_op)
             D_loss, G_loss, summary = sess.run([errD, errG, merged_summary_op])
-         elif LOSS_METHOD == 'gan'or LOSS_METHOD == 'energy':
-            sess.run(D_train_op)
-            sess.run(G_train_op)
-            D_loss, G_loss, summary = sess.run([errD, errG, merged_summary_op])
-         elif LOSS_METHOD == 'cnn':
-            sess.run(G_train_op)
-            loss, summary = sess.run([errG, merged_summary_op])
 
          summary_writer.add_summary(summary, step)
-         if LOSS_METHOD != 'cnn' and step%10==0: print 'epoch:',epoch_num,'step:',step,'D loss:',D_loss,'G_loss:',G_loss,'time:',time.time()-s
-         else:
-            if step%50==0:print 'epoch:',epoch_num,'step:',step,'loss:',loss,' time:',time.time()-s
+         print 'epoch:',epoch_num,'step:',step,'D loss:',D_loss,'G_loss:',G_loss
          step += 1
          
          if step%500 == 0:
